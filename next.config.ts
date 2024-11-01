19a7f8a5b5ec7d336bcf884e6d693355feadf82f
reyraa <ali@muzikie.com>
2024-11-01 16:09:48 +0100

    test:try server side vars

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,17 @@ const nextConfig: NextConfig = {
       {
         protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL || 'http',
         hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || 'localhost',
-        port: process.env.NEXT_PUBLIC_IMAGE_PORT || '',
+        port: process.env.NEXT_PUBLIC_IMAGE_PORT || '3000',
         pathname: '/uploads/**',
       },
     ],
+  },
+  env : {
+    NEXT_PUBLIC_TOKEN_SYMBOL: process.env.NEXT_PUBLIC_TOKEN_SYMBOL,
+    NEXT_PUBLIC_NETWORK_NAME: process.env.NEXT_PUBLIC_NETWORK_NAME,
+    NEXT_PUBLIC_IMAGE_PROTOCOL: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL,
+    NEXT_PUBLIC_IMAGE_HOSTNAME: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME,
+    NEXT_PUBLIC_IMAGE_PORT: process.env.NEXT_PUBLIC_IMAGE_PORT,
   },
 };
 
