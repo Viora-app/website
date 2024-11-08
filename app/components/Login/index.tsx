9d4d25c80abb87cb4a28295620620f6c93bde820
reyraa <ali@muzikie.com>
2024-11-08 14:35:30 +0100

    refactor:use AuthButton to authenticate

--- app/components/Login/index.tsx
+++ app/components/Login/index.tsx
@@ -1,103 +1,24 @@
-'use client'
-
-import React, {FC, useEffect, useState} from 'react';
-import {useRouter} from 'next/navigation';
+import React from 'react';
 import NextImage from 'next/image';
 
-import {Routes} from '@/app/config/routes';
-import {Small, H3, Span, View, TextInput} from '@/app/components/Polyfills';
-import {Button, SafeArea} from '@/app/components/Elements';
-import {useAccount} from '@/app/hooks/useAccount';
-import appLogo from '@/public/images/applogo.png';
+import {H1, View} from '@/app/components/Polyfills';
+import {SafeArea} from '@/app/components/Elements';
+import AuthButton from '@/app/components/AuthButton';
+import appLogo from '@/public/images/app-logo-white.svg';
 
-const ErrorMessage: FC<{errorMessage: string}> = ({errorMessage}) => {
-  if (typeof errorMessage !== 'string' || !errorMessage) {
-    return null;
-  }
-  const networkReg = /network/i;
-  let formattedMessage = errorMessage;
+const Login = () => (
+  <SafeArea className="flex flex-col justify-center items-center">
+    <View className="px-6 w-[510px] flex flex-col justify-center items-center">
+      <View className="p-6 flex flex-row justify-center items-center">
+        <NextImage alt="App Logo" src={appLogo} />
+        <H1 className="font-poppins text-bodyColor text-3xl font-bold pl-4">Viora</H1>
+      </View>
 
-  if (networkReg.test(errorMessage)) {
-    formattedMessage = 'Check your internet connection';
-  } else {
-    formattedMessage = 'The email/password combination was not correct.';
-  }
+      <AuthButton providerName="google" className="mb-2" />
+      <AuthButton providerName="apple" />
 
-  return (
-    <View className="pb-4">
-      <Small className="text-warnStrong">{formattedMessage}</Small>
     </View>
-  );
-};
-
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const {push: navigate} = useRouter();
-  const {signIn, signUp, account, error} = useAccount();
-  const [isNavigating, setIsNavigating] = useState(false);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    signIn(email, password);
-  };
-
-  const onRegister = async () => {
-    signUp(email, password, email);
-  };
-
-  useEffect(() => {
-    if (!!account?.jwt && !isNavigating) {
-      setIsNavigating(true);
-      navigate(Routes.Home as never);
-    }
-  }, [account, isNavigating, navigate]);
-
-  const isButtonDisabled = !email || !password;
-
-  return (
-    <SafeArea className="!bg-neutralPure flex flex-col justify-center items-center">
-      <form className="px-6 w-[510px]" onSubmit={onSubmit}>
-        <View className="p-6 flex flex-row justify-center items-center">
-          <NextImage alt="App Logo" src={appLogo} />
-        </View>
-
-        <H3 className="text-primaryStrong py-6">Login</H3>
-
-        <TextInput
-          onChangeText={setEmail}
-          value={email}
-          placeholder="Email"
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <TextInput
-          onChangeText={setPassword}
-          value={password}
-          placeholder="Password"
-          secureTextEntry
-          autoCapitalize="none"
-          keyboardType="password"
-        />
-
-        <ErrorMessage errorMessage={error} />
-
-        <View className="flex flex-row nowrap gap-6 justify-center items-center">
-          <Button
-            title="Sign in"
-            disabled={isButtonDisabled}
-            type="submit"
-          />
-          <Span>Or</Span>
-          <Button
-            onPress={onRegister}
-            title="Sign up"
-            disabled={isButtonDisabled}
-          />
-        </View>
-      </form>
-    </SafeArea>
-  );
-};
+  </SafeArea>
+);
 
 export default Login;
