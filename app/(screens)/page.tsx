282a06a28bab169740278f2bfa69597012249de1
reyraa <ali@muzikie.com>
2024-11-10 22:32:18 +0100

    refactor:use fetch to get feed data

--- app/(screens)/page.tsx
+++ app/(screens)/page.tsx
@@ -1,23 +1,37 @@
-'use client'
-
 import React from 'react';
 
 import {SafeArea} from '@/app/components/Elements';
 import Feed from '@/app/components/Feed';
-import Form from '@/app/components/Forms/Project/Create';
-import ModalButton from '@/app/components/ModalButton';
-import {ButtonType} from '@/app/components/ModalButton/types';
+import {Icon} from '@/app/components/Elements';
+import {ENDPOINTS} from '@/app/config/endpoints';
+import {Routes} from '@/app/config//routes';
+import {apiClient} from '@/app/utils/api';
+import { Link } from '../components/Polyfills';
 
-const FeedScreen = () => (
-  <SafeArea>
-    <Feed />
-    <ModalButton
-      type={ButtonType.Add}
-      title="Let the world know"
-      description="and receive love and support"
-      modalContent={Form}
-    />
-  </SafeArea>
-);
+const FeedScreen = async () => {
+  let data;
+  try {
+    data = await apiClient(ENDPOINTS.FEED);
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+    // Handle the error (e.g., redirect to login if unauthorized)
+  }
+
+  return (
+    <SafeArea>
+      <Feed data={data} />
+      <Link
+        to={{screen: Routes.CreateProjects}}
+        className="absolute right-8 bottom-8 w-[44px] h-[44px] rounded-3xl bg-assureStrong p-2 shadow-lg hover:shadow-xl cursor-pointer"
+      >
+        <Icon
+          name="plus"
+          size={28}
+          color="#fff"
+        />
+      </Link>
+    </SafeArea>
+  );
+};
 
 export default FeedScreen;
