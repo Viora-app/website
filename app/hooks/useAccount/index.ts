9980c23b88e32e4d926ef7aefea3d00ccf20c170
reyraa <ali@muzikie.com>
2024-11-11 17:20:23 +0100

    refactor:remove unused hooks and context providers

--- app/hooks/useAccount/index.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import {useContext} from 'react';
-import {AccountContext} from '@/app/context/accountContext/AccountContextProvider';
-import {AccountContextType} from '@/app/context/accountContext/types';
-
-export const useAccount = (): AccountContextType => useContext(AccountContext);
