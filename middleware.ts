526325e124a569983997c7881cd10b64f9f06e70
reyraa <ali@muzikie.com>
2024-11-11 17:20:52 +0100

    fix:use cookie name var in the middleware

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,12 @@ import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import type { NextRequest } from 'next/server';
 import {Routes, privateRoutes} from '@/app/config/routes';
+import {AUTH_COOKIE} from '@/app/config/constants';
 
 
 export async function middleware(request: NextRequest) {
   const awaitedCookies = await cookies();
-  const jwt = awaitedCookies.get('jwt')?.value;
+  const jwt = awaitedCookies.get(AUTH_COOKIE)?.value;
 
   if (!jwt && privateRoutes.includes(request.nextUrl.pathname as Routes)) {
     return NextResponse.redirect(new URL(Routes.Login, request.url));
