532d825634e9ad1ddce19ed7bc3810191d88fcbb
reyraa <ali@muzikie.com>
2024-11-17 13:04:41 +0100

    refactor:temporarily disable active state

--- app/components/Polyfills/Link.tsx
+++ app/components/Polyfills/Link.tsx
@@ -1,12 +1,10 @@
 import React, {FC} from 'react';
 import NextLink from 'next/link';
-// import {usePathname} from 'next/navigation';
 
 import View from './View';
 import {LinkProps} from './types';
 
-const Link: FC<LinkProps> = ({children, to, className, disabled, markActive}) => {
-  // const route = usePathname();
+const Link: FC<LinkProps> = ({children, to, className, disabled}) => {
   const pathname = to.screen;
   const searchParams = new URLSearchParams(to.params as Record<string, string>).toString();
   const href = `${pathname}${searchParams ? `?${searchParams}` : ''}`;
