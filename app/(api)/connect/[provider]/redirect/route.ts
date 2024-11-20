b2e9340c1ddd6e9fc7c7aad9c386dfc830b114fc
reyraa <ali@muzikie.com>
2024-11-20 21:04:07 +0100

    fix:add dev logs

--- app/(api)/connect/[provider]/redirect/route.ts
+++ app/(api)/connect/[provider]/redirect/route.ts
@@ -5,11 +5,11 @@ import {apiBaseUrl} from '@/app/config/endpoints';
 import {Routes} from '@/app/config/routes';
 import {AUTH_COOKIE} from '@/app/config/constants';
 
-const domain = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL as string;
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
   path: '/',
-  domain,
+  domain: baseUrl,
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
 };
@@ -19,9 +19,10 @@ export async function GET(
   request: Request,
   params: {params: {provider: string}},
 ) {
-  
   try {
     const {searchParams} = new URL(request.url);
+    console.log('request.url', request.url);
+    console.log('baseUrl', baseUrl);
     const token = searchParams.get('access_token');
     if (!token) {
       throw new Error('Failed to authenticated by provider');
@@ -35,13 +36,14 @@ export async function GET(
     const data = await res.json();
     const awaitedCookie = await cookies();
     awaitedCookie.set(AUTH_COOKIE, data.jwt, config);
-    const redirectionUrl = new URL(Routes.Home, domain);
+    console.log('data.jwt', data.jwt);
+    const redirectionUrl = new URL(Routes.Home, baseUrl);
     console.log('Redirect: Cookies set, redirecting to ', redirectionUrl);
     return NextResponse.redirect(redirectionUrl);
   } catch (error) {
     console.log('Error connecting account', error);
-    const redirectionUrl = new URL(Routes.Login, domain);
-    console.log('Error, redirecting to ', redirectionUrl);
+    const redirectionUrl = new URL(Routes.Login, baseUrl);
+    console.log('Error, redirecting');
     return NextResponse.redirect(redirectionUrl);
   }
 }
