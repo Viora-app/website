d4b26f908ba1f7a11b43e356348ae2e313196d76
curvesy <noobesang@gmail.com>
2024-11-23 18:36:41 +0330

    fix:add dev log

--- middleware.ts
+++ middleware.ts
@@ -17,9 +17,10 @@ export async function middleware(request: NextRequest) {
   const awaitedCookies = await cookies();
   const jwt = awaitedCookies.get(AUTH_COOKIE)?.value;
 
-//   console.log('request', request.url.includes('login'));
+  console.log('jwt', jwt);
+  
   console.log('request', request);
-  console.log('NEXTREQUEST', request.nextUrl);
+//   console.log('NEXTREQUEST', request.nextUrl);
   if (request.nextUrl.pathname.match(/^\/login\/*/)) {
     const urlSearchParams = new URLSearchParams(request.nextUrl.search)
     const params = Object.fromEntries(urlSearchParams.entries());
@@ -32,7 +33,7 @@ export async function middleware(request: NextRequest) {
     
         const res = await fetch(url);
         const data = await res.json();
-        console.log('data', data);
+        console.log('data jwt', data.jwt);
 
         const redirect = NextResponse.redirect(new URL(Routes.Home, request.url))
         redirect.cookies.set(AUTH_COOKIE, data.jwt, {
